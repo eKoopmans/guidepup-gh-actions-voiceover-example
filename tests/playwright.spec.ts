6eaf634c5e095b9f04be2ffd8ed3172047217ea7
cmorten <craig.morten@hotmail.co.uk>
2022-02-17 21:34:37 +0000

    feat: upgrade to latest version of guidepup-playwright

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -1,54 +1,62 @@
 import { expect } from "@playwright/test";
-import { voTest as test } from "@guidepup/playwright";
 import searchJourneyItemTextSnapshot from "./searchJourneyItemTextSnapshot.json";
+import { voTest as test } from "@guidepup/playwright";
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+async function waitForWebContentAnnouncement(voiceOver) {
+  for (let i = 0; i < 10; i++) {
+    const itemText = await voiceOver.itemText();
+
+    if (itemText?.includes("web content")) {
+      return;
+    }
+
+    await delay(50);
+  }
+
+  throw new Error("web content could not be found");
+}
+
 test.describe("Playwright VoiceOver", () => {
   test("I can navigate the Playwright website to the Safari section", async ({
     page,
-    vo,
+    voiceOver,
   }) => {
     await page.goto("https://playwright.dev/", {
       waitUntil: "domcontentloaded",
     });
 
-    // Wait for page to be ready and interact
+    // Wait for page to be ready and interact.
     await expect(page.locator(".navbar__logo")).toBeVisible();
+    await waitForWebContentAnnouncement(voiceOver);
+    await voiceOver.interact();
 
-    while (
-      (await vo.getLastSpokenPhrase())?.includes("Playwright web content")
-    ) {
-      await delay(250);
-      await vo.commandInteractWithItem();
+    // Navigate to the search bar.
+    while (!(await voiceOver.lastSpokenPhrase())?.startsWith("Search")) {
+      await voiceOver.perform(voiceOver.keyboard.commands.findNextControl);
     }
 
-    // Move across the navigation menu to the search bar
-    while (!(await vo.getLastSpokenPhrase())?.startsWith("Search")) {
-      await vo.moveNext();
-    }
-
-    // Search for Safari
-    await page.keyboard.type("Safari");
-    await Promise.all([page.waitForNavigation(), vo.performAction()]);
+    // Search for Safari.
+    // Comboboxes are fiddly to get right... 😅 Seems with the Playwright
+    // website we need to do some arrow keying to get the required focus
+    // on the desired option.
+    await voiceOver.type("Safari");
+    await voiceOver.press("ArrowDown");
+    await voiceOver.press("ArrowUp");
+    await Promise.all([page.waitForNavigation(), voiceOver.act()]);
     expect(page.url()).toBe("https://playwright.dev/docs/browsers#webkit");
 
-    // We're getting there, but seems don't get focus on the section we want!
-    // We have to navigate to the Webkit section. We do this by navigating
-    // by header.
-    while ((await vo.getLastSpokenPhrase()) !== "Headings") {
-      await vo.commandCycleRightThroughNavigationSettings();
-    }
-
-    while ((await vo.getItemText()) !== "WebKit heading level 2") {
-      await vo.commandNavigateDown();
+    // Let's navigate the page to the WebKit section.
+    while ((await voiceOver.itemText()) !== "WebKit heading level 2") {
+      await voiceOver.perform(voiceOver.keyboard.commands.findNextHeading);
     }
 
     // Assert that we've ended up where we expected and what we were told on
     // the way there is as expected.
-    const itemTextLog = await vo.getItemTextLog();
+    const itemTextLog = await voiceOver.itemTextLog();
 
     for (const expectedItem of searchJourneyItemTextSnapshot) {
       expect(itemTextLog).toContain(expectedItem);
